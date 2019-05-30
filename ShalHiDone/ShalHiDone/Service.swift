b514b57080b51d138293414ddddcfd40cbe3a189
hashemi <hashemi6670ali@gmail.com>
2019-05-30 19:04:59 +0430

    done add ok

---
+++ ShalHiDone/ShalHiDone/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  ShalHiDone
+//
+//  Created by shal hi on 5/30/19.
+//  Copyright © 2019 shal hi. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "my name's Shal hi."
+    }
+}
---
+++ ShalHiDone/ShalHiDone/ShalHiDone.h
@@ -0,0 +1,19 @@
+//
+//  ShalHiDone.h
+//  ShalHiDone
+//
+//  Created by shal hi on 5/30/19.
+//  Copyright © 2019 shal hi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShalHiDone.
+FOUNDATION_EXPORT double ShalHiDoneVersionNumber;
+
+//! Project version string for ShalHiDone.
+FOUNDATION_EXPORT const unsigned char ShalHiDoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShalHiDone/PublicHeader.h>
+
+
